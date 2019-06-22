d2e4baa398324ff3d619bb3e6f7e9669dd492a9c
Pavel Tereschenko <me@stupidcat.wtf>
2019-06-22 21:32:49 +0500

    Fix Key

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -1,5 +1,10 @@
 /* @flow */
 
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+import { Root } from '../Root';
+
 export const Key = styled((props: {}) => (
   <Root
     as='span'
